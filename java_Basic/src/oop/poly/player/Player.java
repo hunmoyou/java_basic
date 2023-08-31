29a25d84aa9f7bd69a8978a0f072be887368b947
hunmo <hunmo089300@naver.com>
2023-08-31 19:41:13 +0900

    0831 java

---
+++ java_Basic/src/oop/poly/player/Player.java
@@ -0,0 +1,63 @@
+package oop.poly.player;
+
+public class Player {
+
+	String name;
+	int level;
+	int atk;
+	int hp;
+	
+	Player(){
+		this.level = 1;
+		this.atk  = 3;
+		this.hp = 50;
+	}
+	
+	Player(String name){
+		this(); 
+		System.out.println("Player의 2번 생성자 호출!");
+		this.name = name;
+	}
+	
+	Player(String name, int hp){
+		this(name); //다른 생성자의 호출은 생성자 내에서 최상단에 위치해야 합니다.
+		System.out.println("Player의 3번 생성자 호출");
+		this.hp = hp;
+	}
+	
+	void attack(Player target) {
+		
+		System.out.println("target: " + target);
+		System.out.println("this: " + this);
+		if(this == target) {
+			System.out.println("스스로는 공격할 수 없습니다.");
+			return;
+		}		
+		//출력 메세지: x가 y를 공격합니다.
+		System.out.printf("%s(이)가 %s(을)를 공격합니다.\n"
+				,this.name, target.name);
+		//맞는 사람의 hp를 10 낮추고 나의 체력을 5 회복하고 싶다.
+		//결과를 출력하자.(나의 체력: xx, 상대의 체력: xx)
+		target.hp -= 10; //target.hp = target.hp -10;
+		this.hp +=5;
+		System.out.printf("나의 체력: %d, 상대방 체력: %d\n"
+				,this.hp, target.hp);
+	}
+	
+	void characterInfo() {
+		System.out.println("*** 캐릭터의 정보 ***");
+		System.out.println("# 아이디: "+ name);
+		System.out.println("# 레벨: "+ level);
+		System.out.println("# 공격력: "+ atk);
+		System.out.println("# 체력: "+ hp);
+		System.out.println("이 메서드를 호출한 객체의 이름은: " + this.name);
+	}
+
+	
+}
+
+//내 똥
+//else {
+//System.out.printf("%s가 %s를 공격합니다\n",this.name, target.name);
+//System.out.printf("결과: (나의체력: %d), (상대의 체력: %d)",this.hp+5,target.hp-10);
+//}
